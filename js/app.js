5bfff260f30390463195d9b438bbd1094c08a00e
jhon tobon <jhon.shiru@gmail.com>
2022-06-11 14:58:39 -0500

    fin del algoritmo para saber genero segun nombre

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
- let dato=document.getElementById('dato');
-const submit=document.getElementById('submit');
-const conter=document.getElementById('conter');
+let dato = document.getElementById('dato');
+const submit = document.getElementById('submit');
+const conter = document.getElementById('conter');
 
-let saberGenero=(dato)=>{
-    valid=["1","2","3","4","5","6","7","8","9"];
-    const women= `<h1>Eres mujer</h1>`;
-    const men= `<h1>Eres hombre</h1>`;  
-     genero=dato.split("");
-  
-    let knOw=genero[genero.length -1];
-    let nEWdato=genero[genero.length];
+let saberGenero = (dato) => {
+    valid = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const women = `<h1>${dato}: Eres mujer</h1>`;
+    const men = `<h1>${dato}: Eres hombre</h1>`;
+    genero = dato.split("");
+
+    let knOw = genero[genero.length - 1];
+    let nEWdato = genero[genero.length];
     //inclides no permite validar que el campo input no tenga numeros :/
-    if(dato=="" || valid.includes(knOw)){
-       
+    if (dato == "" || valid.includes(knOw)) {
+
         alert("ingresa un nombre valido en 'ESPAÑOL'")
-           return;
-    }if(knOw == "a"){
-         conter.innerHTML+=women;
-    
-      }else{
-         conter.innerHTML+=men;
-      }
+        return;
+    } if (knOw == "a") {
+        conter.innerHTML += women;
 
-     }
-    
-     //button the event 'click' "permite enviar el contenido del input"
-   submit.addEventListener("click",()=>{
-    let dato=document.getElementById('dato').value;
-       
-       saberGenero(dato);
-       dato.value="";
-   })
-    // Button reload "refresca la pagina"
-   const reload=document.getElementById('reload');
-        reload.addEventListener("click",()=>{
-            window.location.reload(true);
-        });
+    } else {
+        conter.innerHTML += men;
+    }
 
-    
+}
+
+//button the event 'click' "permite enviar el contenido del input"
+submit.addEventListener("click", () => {
+    let dato = document.getElementById('dato').value;
+
+    saberGenero(dato);
+    dato.value = "";
+})
+// Button reload "refresca la pagina"
+const reload = document.getElementById('reload');
+reload.addEventListener("click", () => {
+    window.location.reload(true);
+});
+
+
